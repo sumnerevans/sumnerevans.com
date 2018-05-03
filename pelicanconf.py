003878c7186486c8521d77aafa09dc9010253a14
Sumner Evans <sumner.evans98@gmail.com>
2018-05-03 10:07:33 -0600

    CNAME and Google site verification fix on config

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,7 @@ FAVICON = SITEURL + '/images/favicon.ico'
 CC_LICENSE = {
     'name': 'Creative Commons Attribution-ShareAlike',
     'version': '4.0',
-    'slug': 'by-sa'
+    'slug': 'by-sa',
 }
 
 COPYRIGHT_YEAR = 2017
@@ -30,11 +30,18 @@ CUSTOM_CSS = 'static/custom.css'
 
 DEFAULT_LANG = 'en'
 
-STATIC_PATHS = ['images', 'static', 'extra/google665b11e17b58ce89.html']
+STATIC_PATHS = [
+    'images',
+    'static',
+    'extra',
+]
 EXTRA_PATH_METADATA = {
-    'extra/google665b11e17b58ce89.html': {
-        'path': 'google665b11e17b58ce89.html',
+    'extra/CNAME': {
+        'path': 'CNAME'
     },
+    'extra/google665b11e17b58ce89.html': {
+        'path': 'google665b11e17b58ce89.html'
+    }
 }
 
 ARTICLE_SAVE_AS = '{date:%Y}/{slug}.html'
@@ -44,7 +51,11 @@ PLUGIN_PATHS = ['plugins']
 PLUGINS = ['post_stats', 'neighbors', 'related_posts']
 PYGMENTS_STYLE = 'autumn'
 
-IGNORE_FILES = ['.vim-template:.md', '.vim-template:.rst']
+ARTICLE_EXCLUDES = ['extra']
+IGNORE_FILES = [
+    '.vim-template:.md',
+    '.vim-template:.rst',
+]
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -54,20 +65,27 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Social widget
-SOCIAL = (('linkedin', 'https://www.linkedin.com/in/sumnerevans'),
-          ('stack-overflow', 'http://stackoverflow.com/users/2319844/'),
-          ('github', 'https://github.com/sumnerevans'),
-          ('instagram', 'https://www.instagram.com/sumner.evans/'),
-          ('facebook', 'https://www.facebook.com/sumner.evans98'),
-          ('twitter', 'https://twitter.com/sumner_evans'),
-          ('medium', 'https://medium.com/@sumner.evans'),
-          ('youtube', 'https://www.youtube.com/channel/UCyrdRO4oJRpszr0ovN1FwBA/'),)
+SOCIAL = (
+    ('linkedin', 'https://www.linkedin.com/in/sumnerevans'),
+    ('stack-overflow', 'http://stackoverflow.com/users/2319844/'),
+    ('github', 'https://github.com/sumnerevans'),
+    ('instagram', 'https://www.instagram.com/sumner.evans/'),
+    ('facebook', 'https://www.facebook.com/sumner.evans98'),
+    ('twitter', 'https://twitter.com/sumner_evans'),
+    ('medium', 'https://medium.com/@sumner.evans'),
+    ('youtube', 'https://www.youtube.com/channel/UCyrdRO4oJRpszr0ovN1FwBA/'),
+)
 
-MENUITEMS = (('Archives', '/archives.html'),
-             ('Categories', '/categories.html'),
-             ('Tags', '/tags.html'),)
+MENUITEMS = (
+    ('Archives', '/archives.html'),
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+)
 
-LINKS = (('resume', '/static/resume.pdf'),)
+LINKS = ((
+    'resume',
+    '/static/resume.pdf',
+), )
 
 DEFAULT_PAGINATION = 10
 
