454ae9c9068cde8bf823bd604d4b962cf31a7680
Sumner Evans <sumner.evans98@gmail.com>
2018-05-24 09:40:59 -0600

    Move Flex out of repo

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+# Installs the necessary dependencies.
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+
+# Install the theme.
+git clone https://github.com/alexandrevicenzi/Flex
