bc58841f73268da417cd26449af628345a66b83b
Sumner Evans <sumner.evans98@gmail.com>
2018-07-31 14:41:04 -0600

    Moving to DO

--- cicd/deploy/script.sh
+++ cicd/deploy/script.sh
@@ -6,4 +6,4 @@ eval $(ssh-agent -s)
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
 # Send everything up to sumnerevans.com
-rsync -vr --delete-after output/ sumnerevans@sumnerevans.com:sumnerevans.com
+rsync -vr --delete-after output/ root@sumnerevans.com:/usr/share/nginx/sites/sumnerevans.com
