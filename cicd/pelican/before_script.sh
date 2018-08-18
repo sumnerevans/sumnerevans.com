d5de851333502427fca57c05173265c3f4686b27
Sumner Evans <sumner.evans98@gmail.com>
2018-08-17 22:36:24 -0600

    Remove clone of submodule

--- cicd/pelican/before_script.sh
+++ cicd/pelican/before_script.sh
@@ -5,6 +5,3 @@ set -e
 apk update && apk upgrade && apk add git make
 pip install --upgrade pip
 pip install -r requirements.txt
-
-# Clone in the theme
-git clone https://github.com/alexandrevicenzi/Flex
