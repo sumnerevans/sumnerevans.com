746d0afc83f4dd826ab0fa2cd577afaea8c39056
Sumner Evans <sumner.evans98@gmail.com>
2018-11-23 16:39:48 -0700

    Google Analytics

--- publishconf.py
+++ publishconf.py
@@ -19,5 +19,5 @@ CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
-
+GOOGLE_ANALYTICS = 'UA-104941015-1'
 DISQUS_SITENAME = 'sumnerevans'
