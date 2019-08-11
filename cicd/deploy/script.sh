db361dc20d8bc2ccc5474a915450f274ba20e00f
Sumner Evans <sumner.evans98@gmail.com>
2019-08-11 14:25:59 -0600

    Removed unnecessary CICD scripts

--- cicd/deploy/script.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-set -e
-
-# SSH KEY
-eval $(ssh-agent -s)
-echo "$SSH_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
-
-# Send everything up to sumnerevans.com
-rsync -vr --delete-after output/ root@sumnerevans.com:/usr/share/nginx/sites/sumnerevans.com
--- cicd/pelican/before_script.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-set -e
-
-# Requirements
-apk update && apk upgrade && apk add git make nodejs nodejs-npm
-pip install --upgrade pip
-pip install -r requirements.txt
-npm install -g sass
