a3783e3d63a1968dc16dfed65ebbbbe82056f3db
Sumner Evans <me@sumnerevans.com>
2020-11-13 14:03:44 -0700

    Add use my hugo fork in nix-shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,34 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+    # Use this overlay until https://github.com/gohugoio/hugo/pull/7763 is
+    # merged.
+    overlays = [
+      (
+        self: super: {
+          hugo = super.hugo.overrideAttrs (
+            old: {
+              version = "master";
+              src = self.fetchFromGitHub {
+                owner = "sumnerevans";
+                repo = "hugo";
+                rev = "9bf6ae3ac7a7cc1f48a1e70a2e72dd1c92a11473";
+                sha256 = "1y3j43xjf2ln5xvs1h46iz4k1yf5ql84shri3mcsphfli363nf2n";
+              };
+            }
+          );
+        }
+      )
+    ];
+  }
+}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
+    docutils
+    git
     hugo
+    openssh
+    python38Packages.pygments
+    rnix-lsp
     rst2html5
+    rsync
   ];
 }
