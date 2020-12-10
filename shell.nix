5c26b242546b08927178cf3966dd31e26b4de2bd
Sumner Evans <me@sumnerevans.com>
2020-12-09 17:42:46 -0700

    Fix the nix-shell

--- shell.nix
+++ shell.nix
@@ -10,8 +10,8 @@
               src = self.fetchFromGitHub {
                 owner = "sumnerevans";
                 repo = "hugo";
-                rev = "master";
-                sha256 = "110yzs7k41cdm087gc2s0vabns6ryyspvbkgi9pzvmy70q4gaxnr";
+                rev = "58000536aa1004b90fcc6835262854d37aaaee6e";
+                sha256 = "0gsy5cp06jfkfhc21zy9zf5yiml4mrfi9qdw3px7ivmb86msqjy1";
               };
             }
           );
@@ -28,7 +28,7 @@ pkgs.mkShell {
     openssh
     python38Packages.pygments
     rnix-lsp
-    rst2html5
+    #rst2html5
     rsync
   ];
 }
