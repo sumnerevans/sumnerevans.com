75ab8b3f22e0d05679c8cdbb9e615cc43df14c0d
Sumner Evans <me@sumnerevans.com>
2021-04-06 16:32:49 -0600

    Use openring from nixpkgs

--- shell.nix
+++ shell.nix
@@ -16,20 +16,6 @@ let
     ];
   };
   lib = pkgs.lib;
-
-  openring = with pkgs; buildGoModule rec {
-    pname = "openring";
-    version = "master";
-
-    src = fetchFromSourcehut {
-      owner = "~sircmpwn";
-      repo = pname;
-      rev = "f13edb5dfd882ce608d61cf6b6740650ce9d84a3";
-      sha256 = "sha256-Z65V77JZ9jCzBg7T2+d5Agxxd+MV2R7nYcLedYP5eOE=";
-    };
-
-    vendorSha256 = "sha256-BbBTmkGyLrIWphXC+dBaHaVzHuXRZ+4N/Jt2k3nF7Z4=";
-  };
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
