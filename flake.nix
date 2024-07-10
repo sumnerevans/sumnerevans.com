58bf0b8c2d82ddb8b316fa380504999d209c247e
Sumner Evans <sumner.evans@automattic.com>
2024-07-10 15:11:38 -0600

    flake: update nixpkgs
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- flake.nix
+++ flake.nix
@@ -7,47 +7,36 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    (flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = import nixpkgs {
-            system = system;
+    (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
 
-            # Use this overlay until https://github.com/gohugoio/hugo/pull/7763 is
-            # merged.
-            overlays = [
-              (
-                self: super: {
-                  hugo = super.hugo.overrideAttrs (
-                    old: {
-                      patches = (old.patches or [ ]) ++ [ ./hugo-rst-patches.patch ];
-                    }
-                  );
-                }
-              )
-            ];
-          };
-          ciPackages = with pkgs; [
-            bashInteractive
-            docutils
-            hugo
-            openring
-            python3Packages.pygments
+          # Use this overlay until https://github.com/gohugoio/hugo/pull/7763 is
+          # merged.
+          overlays = [
+            (self: super: {
+              hugo = super.hugo.overrideAttrs (old: {
+                patches = (old.patches or [ ]) ++ [ ./hugo-rst-patches.patch ];
+              });
+            })
           ];
-        in
-        {
-          devShells = {
-            default = pkgs.mkShell {
-              packages = with pkgs; ciPackages ++ [
-                libheif
-                linkchecker
-                nodePackages.htmlhint
-                openssh
-                rsync
-              ];
-            };
-            ci = pkgs.mkShell { packages = ciPackages; };
+        };
+        ciPackages = with pkgs; [
+          bashInteractive
+          docutils
+          hugo
+          openring
+          python3Packages.pygments
+        ];
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs;
+              ciPackages
+              ++ [ libheif linkchecker nodePackages.htmlhint openssh rsync ];
           };
-        }
-      ));
+          ci = pkgs.mkShell { packages = ciPackages; };
+        };
+      }));
 }
