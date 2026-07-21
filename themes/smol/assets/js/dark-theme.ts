2b3051b7257f30d8273b521a7fab627e59f65c32
Sumner Evans <me@sumnerevans.com>
2026-07-21 15:26:04 -0600

    theme: update footer layout and theme switcher and make giscuss update with switcher
    Signed-off-by: Sumner Evans <me@sumnerevans.com>

--- themes/smol/assets/js/dark-theme.ts
+++ themes/smol/assets/js/dark-theme.ts
@@ -1,22 +1,20 @@
-const options = document.getElementById("dark-theme-script")?.dataset;
-const defaultTheme = options?.defaultTheme ?? "light";
-const enableAutoDetectTheme =
-  options?.enableAutoDetectTheme?.toLowerCase() === "true";
+const options = document.querySelector("#dark-theme-script")?.dataset;
+const themeSwitcher = document.querySelector("#theme-switcher");
 
 const darkThemeMatch = window.matchMedia(
-  "(prefers-color-scheme: dark)" +
-  (defaultTheme === "light" ? "" : ",(prefers-color-scheme: no-preference)")
+  "(prefers-color-scheme: dark),(prefers-color-scheme: no-preference)"
 );
 
-const detectThemeAndSwitchStyle = () => {
+const resolveTheme = () => {
   let theme = localStorage.getItem("themeOverride");
   if (theme !== "light" && theme !== "dark") {
-    if (theme === "browser" || enableAutoDetectTheme) {
-      theme = darkThemeMatch.matches ? "dark" : "light";
-    } else {
-      theme = defaultTheme;
-    }
+      return darkThemeMatch.matches ? "dark" : "light";
   }
+  return theme;
+}
+
+const detectThemeAndSwitchStyle = () => {
+  let theme = resolveTheme();
 
   const body = document.body;
   if (theme === "dark") {
@@ -27,7 +25,7 @@ const detectThemeAndSwitchStyle = () => {
     body.classList.remove("dark-theme");
   }
 
-  const notByAILogo = document.getElementById("not-by-ai-logo");
+  const notByAILogo = document.querySelector("#not-by-ai-logo");
   if (notByAILogo) {
     (notByAILogo as HTMLImageElement).src =
       "https://notbyai.fyi/img/written-by-human-not-by-ai-" +
@@ -39,6 +37,19 @@ const detectThemeAndSwitchStyle = () => {
     background:
       theme === "dark" ? "rgba(0, 0, 0, 0.85)" : "rgba(255, 255, 255, 0.9)",
   });
+
+  const giscuss = document.querySelector("iframe.giscus-frame") as HTMLIFrameElement | null;
+  if (giscuss) {
+    giscuss.contentWindow?.postMessage({
+      giscus: {
+        setConfig: { theme },
+      },
+    }, "https://giscus.app");
+  }
+
+  document.querySelector("#theme-switcher .link.dark")?.classList.toggle("disabled", localStorage.getItem("themeOverride") === "dark");
+  document.querySelector("#theme-switcher .link.light")?.classList.toggle("disabled", localStorage.getItem("themeOverride") === "light");
+  document.querySelector("#theme-switcher .link.browser")?.classList.toggle("disabled", ["browser", null].includes(localStorage.getItem("themeOverride")));
 };
 
 detectThemeAndSwitchStyle();
@@ -50,25 +61,38 @@ window.theme = {
   },
 };
 
-// If theme auto-detection is enabled, then add a listenr on the matchMedia.
-if (enableAutoDetectTheme) {
-  darkThemeMatch.addEventListener("change", detectThemeAndSwitchStyle);
+darkThemeMatch.addEventListener("change", detectThemeAndSwitchStyle);
+
+if (themeSwitcher) {
+  themeSwitcher.classList.remove("hidden");
+  for (const theme of ["dark", "light", "browser"]) {
+    themeSwitcher
+      .querySelector(`.${theme}`)
+      ?.addEventListener("click", (e) =>
+        !(e.currentTarget as HTMLElement).classList.contains("disabled")
+        && window.theme.switch(theme)
+      );
+  }
 }
 
-const themeSwitcher = document.getElementById("theme-switcher");
-if (themeSwitcher) {
-  const mkLink = (theme: string) => {
-    const link = document.createElement("span");
-    link.classList.add("link");
-    link.appendChild(document.createTextNode(theme));
-    link.addEventListener("click", () => window.theme.switch(theme));
-    return link;
-  };
-  themeSwitcher.appendChild(document.createTextNode("Switch to the "));
-  themeSwitcher.appendChild(mkLink("dark"));
-  themeSwitcher.appendChild(document.createTextNode(" | "));
-  themeSwitcher.appendChild(mkLink("light"));
-  themeSwitcher.appendChild(document.createTextNode(" | "));
-  themeSwitcher.appendChild(mkLink("browser"));
-  themeSwitcher.appendChild(document.createTextNode(" theme."));
-}
+// Dynamically load Giscus script with the correct theme
+const giscusAttributes = {
+  "src": "https://giscus.app/client.js",
+  "data-repo": "sumnerevans/sumnerevans.com",
+  "data-repo-id": "R_kgDOGejfWA",
+  "data-category": "Announcements",
+  "data-category-id": "DIC_kwDOGejfWM4DAkOg",
+  "data-mapping": "pathname",
+  "data-strict": "1",
+  "data-reactions-enabled": "1",
+  "data-emit-metadata": "0",
+  "data-input-position": "top",
+  "data-theme": resolveTheme(),
+  "data-lang": "en",
+  "crossorigin": "anonymous",
+  "async": "",
+};
+
+const giscusScript = document.createElement("script");
+Object.entries(giscusAttributes).forEach(([key, value]) => giscusScript.setAttribute(key, value));
+document.querySelector("aside.comments")?.appendChild(giscusScript);
